b714e37b78d4f3cd18b552cfa47041291725030c
david-peretz <david82761@gmail.com>
2020-01-14 17:13:02 +0200

    start design

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -14,6 +14,7 @@ const routes: Routes = [
   },
   {
     path: "suppliers",
+    canActivate: [AuthGuard],
     loadChildren: () =>
       import("./modules/suppliers/suppliers.module").then(
         m => m.SuppliersModule
