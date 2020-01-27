29339013a4b11ddc099aecd13e6e6c4e2ab0f8be
david-peretz <david82761@gmail.com>
2020-01-27 14:05:56 +0200

    Auto-generated commit

--- app/modules/contact/services/dto/resource.ts
+++
@@ -1,8 +0,0 @@
-import { Resource } from "src/app/core/services/generic/resource";
-
-export class User extends Resource {
-  name: string;
-  address: string;
-  phone: string;
-  email: string;
-}
